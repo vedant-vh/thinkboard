3ee03fd8f689c127e7b5639f048d4eae749c5b3c
vedant-vh <vedanthadap123@gmail.com>
2026-01-09 17:31:56 +0530

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,13 +4,25 @@ import { connectDB } from "./config/db.js"
 import dotenv from "dotenv"
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from 'cors'
+import path from "path"
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve();
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 
+if (process.env.NODE_ENV !== "production") {
+    
+    app.use(cors({
+        origin:"http://localhost:5173",
+    }));
+
+}
 
 //middleware
 app.use(express.json()); // will parse json bodies: req.body i.e allows us to use {title,content}
@@ -21,14 +33,18 @@ app.use(express.json()); // will parse json bodies: req.body i.e allows us to us
 //     next();
 // })
 
-
-app.use(cors({
-    origin:"http://localhost:5173",
-}));
-
 app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes)
+
+if (process.env.NODE_ENV === "production") {
+     app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+  });
+}
+
 
 connectDB().then(() => {
     app.listen(PORT, () => {
